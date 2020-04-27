f67dc68e0d5da2c3b8a8495b7f8878dc126baa50
whatisthenote <jgj_junior@yahoo.com.br>
2020-04-27 19:12:38 -0300

    controller and view

---
+++ MVC APP/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_APP.Models;
+
+namespace MVC_APP.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name="NAME" };
+            return View(movie);
+        }
+    }
+}
